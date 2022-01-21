322cbfd90bde7052b96fbe70b49e9f5a9d98cc66
mocha <sakuranomiyu@gmail.com>
2022-01-21 13:17:03 +0900

    demo

--- demo.java
+++ demo.java
@@ -4,7 +4,10 @@
 public class FooModel {
     String name;
 
-    public void setName(String name) {
+    public FooModel (String name) {
+        if (name == null) {
+            throw Exception;
+        }
         this.name = name;
     }
 
