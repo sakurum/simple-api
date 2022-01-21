814ce5050d69a23b7b617a06d35d04b8d5a197fa
mocha <sakuranomiyu@gmail.com>
2022-01-21 13:11:28 +0900

    demo

---
+++ demo.java
@@ -0,0 +1,14 @@
+/**
+ * Foo
+ */
+public class FooModel {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ demo2.java
@@ -0,0 +1,9 @@
+
+public class Whoo {
+    public void main() {
+
+        model = new FooModel;
+        model.setName("foo");
+
+    }
+}
