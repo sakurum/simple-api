8db5eb2db57412458052facd0d02d1709a7697c0
mocha <sakuranomiyu@gmail.com>
2022-01-21 13:41:54 +0900

    Update demo3.java

--- demo3.java
+++ demo3.java
@@ -4,12 +4,23 @@
  * @param date
  * @return
  */
-public static boolean isTodayOrFreshnessSystemDate(final Date date) {
+public static boolean isTodayOrAfterSystemDate(Date date) {
     if (date != null
             && DateUtils.isSameYearMonthDate(date, DateUtils.getSystemDate())
             || DateUtils.isFreshnessThanOrEqualToBaseDate(DateUtils.getSystemDate(), date)) {
         return true;
     } else {
         return false;
+    }
+}
+
+
+public void main {
+
+
+
+    if (isTodayOrAfterSystemDate(date)) {
+        // foo
+        //
     }
 }
